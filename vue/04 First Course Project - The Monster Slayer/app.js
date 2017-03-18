43ea614ce3fbecdd89ac5d262ba8bf6c6c2d56f7
drafterleo <drafterleo@gmail.com>
2017-03-18 15:07:29 +0300

    checkWin

--- vue/04 First Course Project - The Monster Slayer/app.js
+++ vue/04 First Course Project - The Monster Slayer/app.js
@@ -14,20 +14,29 @@ new Vue ({
         calcDamage: function (maxDamage, minDamage) {
             return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
         },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else
+            if (this.playerHealth <= 0) {
+                if (confirm('Ypu lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        },
         attack: function () {
             this.monsterHealth -= this.calcDamage(10, 2);
-            if (this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
-                return;
-            }
-
             this.playerHealth -= this.calcDamage(12, 3);
-            if (this.playerHealth <= 0) {
-                alert('You lost!');
-                this.gameIsRunning = false;
-                return;
-            }
+            this.checkWin();
         },
         specialAttack: function () {
 
