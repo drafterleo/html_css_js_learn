f681e3b8940481f5641f4315f8afab532dc04062
drafterleo <drafterleo@gmail.com>
2017-03-18 14:56:09 +0300

    attack function

--- vue/04 First Course Project - The Monster Slayer/app.js
+++ vue/04 First Course Project - The Monster Slayer/app.js
@@ -11,5 +11,32 @@ new Vue ({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
+        calcDamage: function (maxDamage, minDamage) {
+            return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+        },
+        attack: function () {
+            this.monsterHealth -= this.calcDamage(10, 2);
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            this.playerHealth -= this.calcDamage(12, 3);
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+            this.gameIsRunning = false;
+        }
     }
 });
