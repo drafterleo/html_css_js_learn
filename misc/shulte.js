b5c64ae655b7131986190d4bc281b8499fd43043
drafterleo <drafterleo@gmail.com>
2017-03-21 00:34:39 +0300

    show trace

--- misc/shulte.js
+++ misc/shulte.js
@@ -1,34 +1,41 @@
 var appData =  {
     gridSize: 5,
     gridRange: [0, 1, 2, 3, 4],
-    rowHeight: '20%',
-    colWidth: '20%',
-    showHover: true,
-    showSelect: true,
+    cells: [],
+    trace: [],
+    currNum: 1,
+
     hoverIndex: -1,
     selectIndex: -1,
-    correctIndex: 2,
-    selectTimeOut: 500
+    correctIndex: -1,
+
+    showHover: true,
+    showSelect: true,
+    showTrace: true,
+
+    rowHeight: '20%',
+    colWidth: '20%',
+    selectTimeOut: 500,
+    selectedTimerId: -1
 };
 
 new Vue ({
-   el: '#app',
-   data: appData,
+    el: '#app',
+    data: appData,
+    created: function () {
+        this.initGame();
+    },
     watch: {
        gridSize: function (val) {
            if (isNaN(parseInt(val))) {
                this.gridSize = 2; // recursion !!!
                return;
            }
-           //this.gridRange = Array.from({length: val}, function (v, k) {return k});
-           var newRange = [];
-           for (var i = 0; i < val; i++) {
-               newRange.push(i);
-           }
-           this.gridRange = newRange;
+           this.gridRange = this.makeRange(0, val - 1);
            this.rowHeight = 100/val + '%';
            this.colWidth = 100/val + '%';
-           //console.log(val, this.rowHeight, this.colWidth);
+
+           this.initGame();
        }
     },
     computed: {
@@ -37,9 +44,11 @@ new Vue ({
                return this.selectIndex;
            },
            set: function (cellIdx) {
-               this.showSelect = true;
                this.selectIndex = cellIdx;
-               setTimeout(this.hideSelect, this.selectTimeOut);
+               this.showSelect = true;
+               clearTimeout(this.selectedTimerId);
+               this.selectedTimerId = setTimeout(this.hideSelect, this.selectTimeOut);
+               this.nextTurn();
                //console.log('selectedCell: ' + this.selectIndex);
            }
        },
@@ -54,9 +63,58 @@ new Vue ({
         }
     },
     methods: {
+       initGame: function () {
+           this.clearIndexes();
+           this.currNum = 1;
+           this.cells = this.makeRange(1, this.gridSize * this.gridSize);
+           this.trace = [];
+           this.shuffleCells(1000);
+           console.log('init game');
+       },
+       clearIndexes: function () {
+           this.hoverIndex = -1;
+           this.selectIndex = -1;
+           this.correctIndex = -1;
+       },
+       nextTurn: function () {
+            if (this.selectedCell >= 0 && this.selectedCell < this.cells.length) {
+                if (this.cells[this.selectedCell] === this.currNum) {      // correct answer
+                    this.correctIndex = this.cells.indexOf(this.currNum);
+                    this.currNum ++;
+                    if (this.currNum > this.cells.length) {
+                        this.clearIndexes();
+                        clearTimeout(this.selectedTimerId);
+                        //alert('Game Over!');
+                        console.log('game over!')
+                        setTimeout(this.initGame, 1000);
+                    }
+                }
+            }
+       },
+       tracedCell: function (cellIdx) {
+           return this.cells[cellIdx] < this.currNum
+       },
+       makeRange: function (begin, end) {
+           //range = Array.from({length: val}, (v, k) => k);
+           let range = [];
+           for (let i = begin; i <= end; i++) {
+               range.push(i);
+           }
+           return range;
+       },
+       shuffleCells: function (shuffleCount) {
+            for (let i = 0; i < shuffleCount; i++) {
+                let aIdx = Math.floor(Math.random() * this.cells.length);
+                let bIdx = Math.floor(Math.random() * this.cells.length);
+                let aVal = this.cells[aIdx];
+                this.cells[aIdx] = this.cells[bIdx];
+                this.cells[bIdx] = aVal;
+            }
+       },
        hideSelect: function () {
            this.showSelect = false;
            //console.log('select time  out');
        }
+
     }
 });
