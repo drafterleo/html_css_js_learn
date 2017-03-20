394aee5cdd7b420ad90144f54aefe5d81486dd67
drafterleo <drafterleo@gmail.com>
2017-03-20 03:24:54 +0300

    shulte css and events

--- misc/shulte.js
+++ misc/shulte.js
@@ -2,6 +2,7 @@ new Vue ({
    el: '#app',
    data: {
        gridSize: 5,
+       gridRange: [0, 1, 2, 3, 4],
        rowHeight: '20%',
        colWidth: '20%',
        showHover: true,
@@ -10,18 +11,16 @@ new Vue ({
     },
     watch: {
        gridSize: function (val) {
+           if (isNaN(parseInt(val))) {
+               this.gridSize = 2; // recursion !!!
+               return;
+           }
+           this.gridRange = Array.from({length: val}, (v, k) => k);
            this.rowHeight = 100/val + '%';
            this.colWidth = 100/val + '%';
-           console.log(val, this.rowHeight, this.colWidth);
+           //console.log(val, this.rowHeight, this.colWidth);
        }
     },
     methods: {
-        // setGridSize: function (event) {
-        //     var val = Number(event.target.value);
-        //     this.gridSize = val;
-        //     this.rowHeight = 100/val + '%';
-        //     this.colWidth = 100/val + '%';
-        //     console.log(val, this.rowHeight, this.colWidth);
-        // }
     },
 });
