3904c1ff502e00d11f438dba88761b5dbedf4cb4
drafterleo <drafterleo@gmail.com>
2017-03-25 23:30:37 +0300

    component code: functional data + template

--- vue/07 An Introduction to Components/simple/app.js
+++ vue/07 An Introduction to Components/simple/app.js
@@ -1,3 +1,20 @@
-/**
- * Created by Drafter on 25.03.2017.
- */
+Vue.component ('my-cmp', {
+    data: function () {
+        return {
+            status: 'Critical'
+        }
+    },
+    template: '<p>Server status: {{ status}} ' +
+              '(<button @click="changeStatus">Change</button>)</p>',
+    methods: {
+        changeStatus: function () {
+            this.status = ' Normal';
+        }
+    }
+
+
+})
+
+new Vue ({
+    el: '#app'
+});
