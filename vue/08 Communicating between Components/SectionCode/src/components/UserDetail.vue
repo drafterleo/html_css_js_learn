c76abc62194db9b161e9a6fc7dc7f05d14c7c527
drafterleo <drafterleo@gmail.com>
2017-04-02 20:21:38 +0300

    switchName

--- vue/08 Communicating between Components/SectionCode/src/components/UserDetail.vue
+++ vue/08 Communicating between Components/SectionCode/src/components/UserDetail.vue
@@ -2,10 +2,20 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+    export default {
+        props: ['userName'],
+        methods: {
+            switchName: function () {
+                // https://learn.javascript.ru/array-methods
+                return this.userName.split("").reverse().join("");
+            }
+        }
+    }
 </script>
 
 <style scoped>
