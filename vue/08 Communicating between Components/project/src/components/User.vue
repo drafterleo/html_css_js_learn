cce3049a9047719ef84ff7c812ee71db4b606901
drafterleo <drafterleo@gmail.com>
2017-04-10 01:09:45 +0300

    passing events child <-> child

--- vue/08 Communicating between Components/project/src/components/User.vue
+++ vue/08 Communicating between Components/project/src/components/User.vue
@@ -8,11 +8,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <app-user-detail :userName="name"
-                                 v-on:nameWasReset="name = $event">
+                                 @nameWasReset="name = $event"
+                                 :resetFn="resetName()"
+                                 :userAge="age">
                 </app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age"
+                               @ageWasEdited="age = $event">
+                </app-user-edit>
             </div>
         </div>
     </div>
@@ -25,13 +29,17 @@
     export default {
         data: function () {
             return {
-                name: 'Oleg'
+                name: 'Oleg',
+                age: 27
             };
         },
         methods: {
             changeName: function () {
                 this.name = 'Oksana';
-            }    
+            },
+            resetName: function () {
+                this.name = 'Oleg';
+            }
         },
         components: {
             appUserDetail: UserDetail,
