28650222a186bb4fc5f71b8a693c192b14104746
drafterleo <drafterleo@gmail.com>
2017-04-12 00:34:03 +0300

    dynamic components, <component> tag

--- vue/09 Advanced Component Usage/project/src/App.vue
+++ vue/09 Advanced Component Usage/project/src/App.vue
@@ -2,10 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote>
-                    <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p slot="content">A wonderful Quote!</p>
-                </app-quote>
+                <button @click="selectedComponent = 'app-quote'">Quote</button>
+                <button @click="selectedComponent = 'app-author'">Author</button>
+                <button @click="selectedComponent = 'app-new'">New</button>
+                <p>{{ selectedComponent }}</p>
+                <hr>
+                <component :is="selectedComponent">
+                    <p>Default Content...</p>
+                </component>
+                <!--<app-quote>-->
+                    <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
+                    <!--<p slot="content">A wonderful Quote!</p>-->
+                <!--</app-quote>-->
+
             </div>
         </div>
     </div>
@@ -13,15 +22,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function () {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'app-quote'
             }
         },
         components: {
-            'app-quote': Quote
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New
         }
     }
 </script>
